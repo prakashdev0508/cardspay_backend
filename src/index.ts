bce47b2278157ce51455e915feeb1be954a7fd75
Prakash Pandey <prakash.pandey@your-space.in>
2025-02-03 14:57:05 +0530

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import express from "express";
+import { prisma } from "./utils/db";
 
 const app = express();
 app.use(express.json());
- 
+
 app.get("/", async (req, res) => {
-  try { 
+  try {
     res.json({ message: "Working fine" });
   } catch (error) {
-    console.log(error);   
-  } 
+    console.log(error);
+  }
 });
 
-app.get("/get-data", async (req, res) => {
-  try { 
-    res.json({ message: "Working fine get data " });
-  } catch (error) {
-    console.log(error);   
-  } 
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: { name, email },
+  });
+  res.json({message : "User created successfully" , user});
 });
 
- 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
