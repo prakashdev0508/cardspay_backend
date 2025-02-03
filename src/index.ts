09b42cfeb1f2531fb4a50739db004e209626905f
Prakash Pandey <prakash.pandey@your-space.in>
2025-02-03 13:01:19 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+ 
+app.get("/", async (req, res) => {
+  try { 
+    res.json({ message: "Working fine" });
+  } catch (error) {
+    console.log(error);   
+  }
+});
+
+ 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
