7689007553282e84df426697721ab7f0b1dbd690
Prakash Pandey <prakash.pandey@your-space.in>
2025-02-08 12:59:19 +0530

    Added bank name validation

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import e, { NextFunction, Request, Response } from "express";
 import { prisma } from "../utils/db";
 import { createError, createSuccess } from "../utils/resMessage";
 
@@ -38,12 +38,40 @@ export const createNewCard = async (
       data: {
         name: String(name).trim(),
         created_by: userId,
+        bankId: bankId,
       },
     });
 
     createSuccess(res, "card created successfully ", { id: service.id });
   } catch (error) {
     next(createError(500, "Error creating service"));
+  }
+};
+
+export const getCardDetailsByBank = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { bankId } = req.body;
+
+    if (!bankId) {
+      return next(createError(400, "Bank ID is required"));
+    }
+
+    const cardDetails = await prisma.cardsDetails.findMany({
+      where: {
+        bankId: bankId,
+        NOT: {
+          is_deleted: true,
+        },
+      },
+    });
+
+    createSuccess(res, "card details fetched successfully ", cardDetails);
+  } catch (error) {
+    next(createError(500, "Error fetching card details"));
   }
 };
 
@@ -63,6 +91,11 @@ export const getCardDetails = async (
         id: true,
         name: true,
         createdAt: true,
+        bank: {
+          select: {
+            name: true,
+          },
+        },
         createdBy: {
           select: {
             name: true,
