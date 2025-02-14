5b4572110a5fe9757c5db00fef95b965ec86476f
Prakash Pandey <prakash.pandey@your-space.in>
2025-02-14 17:35:21 +0530

    Added number validation

--- src/controllers/transection.ts
+++ src/controllers/transection.ts
@@ -165,7 +165,7 @@ export const updateTransaction = async (
     };
 
     if (req.body.bill_amount !== undefined) {
-      updateData.bill_amount = req.body.bill_amount;
+      updateData.bill_amount = Number(req.body.bill_amount);
     }
     if (req.body.due_date) {
       updateData.due_date = new Date(req.body.due_date);
